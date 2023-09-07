b01727ff256d86b8703e1cb274f0d0f85e365a3c
uooɾ <36707871+wnsdbs@users.noreply.github.com>
2023-09-06 21:19:22 -0400

    Create convertString.java

---
+++ convertString.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class convertstring{
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Input any binary number: ");
+		System.out.print("The demical integer is " + binaryConversion(s.nextLine()) + ".");
+	}
+		
+	public static int binaryConversion(String b)
+	{
+		int total = 0;
+		int length = b.length() - 1;
+		for(int i = 0; i <= b.length() - 1; i++){
+			int p = Integer.parseInt(b.substring(i, i + 1));
+			total += p * Math.pow(2, length);
+			length--;
+		}
+		return total;		
+	}
+}
