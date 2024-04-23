b3d80489e2ec40892e0b590f0468516dc988a784
uooɾ <36707871+wnsdbs@users.noreply.github.com>
2024-04-22 20:18:51 -0400

    Update convertString.java

--- convertString.java
+++ convertString.java
@@ -8,8 +8,7 @@ public class convertstring{
 		System.out.print("The demical integer is " + binaryConversion(s.nextLine()) + ".");
 	}
 		
-	public static int binaryConversion(String b)
-	{
+	public static int binaryConversion(String b){
 		int total = 0;
 		int length = b.length() - 1;
 		for(int i = 0; i <= b.length() - 1; i++){
