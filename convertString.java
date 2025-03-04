eb4618c0670f072afb9567af753be741e3e2be54
ᴅᴀɴɪᴇʟ ʏᴏᴏɴ <danieljyoon7@gmail.com>
2025-03-04 09:25:13 -0500

    Update convertString.java

--- convertString.java
+++ convertString.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class convertstring{
+public class convertstring {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -8,7 +8,7 @@ public class convertstring{
 		System.out.print("The demical integer is " + binaryConversion(s.nextLine()) + ".");
 	}
 		
-	public static int binaryConversion(String b){
+	public static int binaryConversion(String b) {
 		int total = 0;
 		int length = b.length() - 1;
 		for(int i = 0; i <= b.length() - 1; i++){
